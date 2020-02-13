7e82e0fd1f8f9f758df2aa76deaf39dc12f9ab40
OlegPivkin <44081467+OlegPivkin@users.noreply.github.com>
2020-02-13 11:34:51 +0300

    Написал краткие комментарии к каждой функции.

--- js/stat.js
+++ js/stat.js
@@ -1,25 +1,35 @@
 'use strict';
 
+// Параметры облака
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_COLOR = '#FFF';
+
+// Параметры тени облака
 var SHADOW_SLIP = 10;
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+
+// Параметры сообщения
 var TEXT_WIN_MESSAGE = 'Ура вы победили!';
 var TEXT_LIST_RESULTS = 'Список результатов:';
 var TEXT_COLOR = '#000';
 var TEXT_FONT = '16px PT Mono';
 var TEXT_LINE_SPACING = 20;
+
+// Смещение текста
 var SLIP_X = 50;
 var SLIP_Y = 40;
+
+// Параметры гистограммы
 var BAR_GRAPH_HEIGHT = 150;
 var BAR_GRAPH_WIDTH = 40;
 var BAR_GRAPH_SPACING = 50;
 var CURRENT_PLAYER_NAME = 'Вы';
 var CURRENT_PLAYER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
 
+// Получаю цвет гистограммы для player
 var getPlayerColor = function (playerName) {
   if (playerName === CURRENT_PLAYER_NAME) {
     return CURRENT_PLAYER_BAR_COLOR;
@@ -27,17 +37,20 @@ var getPlayerColor = function (playerName) {
   return 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, ' + '77%)';
 };
 
+// Рисую облако и тени
 var renderCTX = function (ctx, color, x, y) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+// Отрисовка сообщения
 var renderText = function (ctx, text, x, y) {
   ctx.fillStyle = TEXT_COLOR;
   ctx.font = TEXT_FONT;
   ctx.fillText(text, x, y);
 };
 
+// Максимальное время прохождения игры
 var getMaxResult = function (results) {
   var maxResult = 0;
 
@@ -49,6 +62,7 @@ var getMaxResult = function (results) {
   return Math.floor(maxResult);
 };
 
+// Отрисовка гистограммы
 var renderHistogram = function (ctx, playerName, playerTime, maxTime, count) {
   var pointX = CLOUD_X + SLIP_X + (BAR_GRAPH_WIDTH + BAR_GRAPH_SPACING) * count;
   var pointY = CLOUD_Y + CLOUD_HEIGHT;
@@ -61,11 +75,14 @@ var renderHistogram = function (ctx, playerName, playerTime, maxTime, count) {
   ctx.fillRect(pointX, pointY - TEXT_LINE_SPACING * 2, BAR_GRAPH_WIDTH, -BAR_GRAPH_HEIGHT * ratioPlayer);
 };
 
+// Результат успешного прохождения уровня
 window.renderStatistics = function (ctx, players, times) {
-  renderCTX(ctx, SHADOW_COLOR, CLOUD_X + SHADOW_SLIP, CLOUD_Y + SHADOW_SLIP);
-  renderCTX(ctx, CLOUD_COLOR, CLOUD_X, CLOUD_Y);
-  renderText(ctx, TEXT_WIN_MESSAGE, SLIP_X * 3, SLIP_Y);
-  renderText(ctx, TEXT_LIST_RESULTS, SLIP_X * 3, SLIP_Y + TEXT_LINE_SPACING);
+  renderCTX(ctx, SHADOW_COLOR, CLOUD_X + SHADOW_SLIP, CLOUD_Y + SHADOW_SLIP); // Тень
+  renderCTX(ctx, CLOUD_COLOR, CLOUD_X, CLOUD_Y); // Облако
+  renderText(ctx, TEXT_WIN_MESSAGE, SLIP_X * 3, SLIP_Y); // Победное сообщение
+  renderText(ctx, TEXT_LIST_RESULTS, SLIP_X * 3, SLIP_Y + TEXT_LINE_SPACING); // Сообщение о результатах
+
+  // Вывод статистики по игрокам
   for (var i = 0; i < players.length; i++) {
     renderHistogram(ctx, players[i], times[i], getMaxResult(times), i);
   }
